ae4b43057df7c4e824676f9e2dc37017512d1e7e
Nguyễn Văn Lân 20IT160 <90605381+panadollr@users.noreply.github.com>
2023-11-18 09:48:49 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../boostrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
